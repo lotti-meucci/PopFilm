e4748b6b2f4fefccb4b9fe3e7b5eb6d90a714d3e
wilun-meucci <85952681+wilun-meucci@users.noreply.github.com>
2023-04-12 08:42:39 +0200

    Update index.js
    bastardi

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const igIcons = document.querySelectorAll('.i-ig')
 const langBtns = document.querySelectorAll('.lang-btn')
 const navLinks = nav.querySelectorAll('a')
 
-const phone = "3396356819"
+const phone = "3776703932"
 localStorage.lang ??= 'it'
 document.documentElement.lang = localStorage.lang
 addEventListener('resize', closeMenu)
